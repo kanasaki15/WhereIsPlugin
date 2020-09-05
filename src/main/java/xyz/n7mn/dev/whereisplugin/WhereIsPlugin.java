b05df08185fe8cee3405ccbf66e76aae09282995
kanasaki15 <kassi_0123@kassi-hp.info>
2020-09-05 17:26:56 +0900

    仕様決め直しする

--- src/main/java/xyz/n7mn/dev/whereisplugin/WhereIsPlugin.java
+++ src/main/java/xyz/n7mn/dev/whereisplugin/WhereIsPlugin.java
@@ -10,8 +10,6 @@ public final class WhereIsPlugin extends JavaPlugin {
         // Plugin startup logic
         Metrics metrics = new Metrics(this, 8481);
 
-        // CREATE TABLE `WhereisList` ( `ID` INT NOT NULL , `WhereName` TEXT NOT NULL , `WorldName` TEXT NOT NULL , `StartX` INT NOT NULL , `StartZ` INT NOT NULL , `EndX` INT NOT NULL , `EndZ` INT NOT NULL , `CreateUser` VARCHAR(36) NOT NULL , `Active` BOOLEAN NOT NULL );
-
         getLogger().info("WhereIsPlugin Started!");
     }
 
--- src/main/java/xyz/n7mn/dev/whereisplugin/api/v2/WhereIsDataAPI.java
+++ src/main/java/xyz/n7mn/dev/whereisplugin/api/v2/WhereIsDataAPI.java
@@ -4,103 +4,19 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.UUID;
 
 public class WhereIsDataAPI {
 
-    private Plugin plugin;
-    private Connection con;
+    final private Plugin plugin;
 
     public WhereIsDataAPI(){
         plugin = null;
-        con = new ConnectSystem().getMySQLConnect((String) ConfigAPI.getConfig("MySQLServer").getValue(), (String) ConfigAPI.getConfig("MySQLUsername").getValue(), (String) ConfigAPI.getConfig("MySQLPassword").getValue(), (String) ConfigAPI.getConfig("MySQLDatabase").getValue(), (String) ConfigAPI.getConfig("").getValue());
     }
 
     public WhereIsDataAPI(Plugin plugin){
         this.plugin = plugin;
-        con = new ConnectSystem().getMySQLConnect((String) ConfigAPI.getConfig("MySQLServer").getValue(), (String) ConfigAPI.getConfig("MySQLUsername").getValue(), (String) ConfigAPI.getConfig("MySQLPassword").getValue(), (String) ConfigAPI.getConfig("MySQLDatabase").getValue(), (String) ConfigAPI.getConfig("").getValue());
     }
 
-    public WhereisData getData(int id){
-        if (plugin == null){
-            throw new NullPointerException();
-        }
-
-        try {
-            boolean isJson = false;
-            if (new ConnectSystem().isMySQLConnect(con)){
-                isJson = true;
-            }
-
-            if (!isJson){
-                PreparedStatement statement = con.prepareStatement("SELECT * FROM WhereisList WHERE WhereID = ?");
-                statement.setInt(1, id);
-                ResultSet set = statement.executeQuery();
-                if (set.next()){
-                    WhereisData data = new WhereisData(
-                            set.getInt("ID"),
-                            set.getString("WhereName"),
-                            Bukkit.getServer().getWorld(set.getString("WorldName")),
-                            set.getInt("StartX"),
-                            set.getInt("StartZ"),
-                            set.getInt("EndX"),
-                            set.getInt("EndZ"),
-                            UUID.fromString(set.getString("CreateUser")),
-                            set.getBoolean("Active")
-                    );
-
-                    return data;
-                }
-            }
-
-            return new WhereisData();
-        } catch (Exception e){
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    public List<WhereisData> getDataList(){
-        if (plugin == null){
-            throw new NullPointerException();
-        }
-
-        try {
-            boolean isJson = false;
-            if (new ConnectSystem().isMySQLConnect(con)){
-                isJson = true;
-            }
-
-            if (!isJson){
-                PreparedStatement statement = con.prepareStatement("SELECT * FROM WhereisList");
-                ResultSet set = statement.executeQuery();
-                List<WhereisData> list = new ArrayList<>();
-                while (set.next()){
-                    WhereisData data = new WhereisData(
-                            set.getInt("ID"),
-                            set.getString("WhereName"),
-                            Bukkit.getServer().getWorld(set.getString("WorldName")),
-                            set.getInt("StartX"),
-                            set.getInt("StartZ"),
-                            set.getInt("EndX"),
-                            set.getInt("EndZ"),
-                            UUID.fromString(set.getString("CreateUser")),
-                            set.getBoolean("Active")
-                    );
-                    list.add(data);
-                }
-                return list;
-            }
-
-            return Collections.EMPTY_LIST;
-        } catch (Exception e){
-            e.printStackTrace();
-            return Collections.EMPTY_LIST;
-        }
-    }
 }
