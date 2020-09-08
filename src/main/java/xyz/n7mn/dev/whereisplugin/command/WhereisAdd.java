28611de69a0b0e1dfbdf6a161029959bce94e881
kanasaki15 <kassi_0123@kassi-hp.info>
2020-09-08 22:13:18 +0900

    いらないimport削除

--- src/main/java/xyz/n7mn/dev/whereisplugin/command/WhereisAdd.java
+++ src/main/java/xyz/n7mn/dev/whereisplugin/command/WhereisAdd.java
@@ -2,15 +2,11 @@ package xyz.n7mn.dev.whereisplugin.command;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
 import xyz.n7mn.dev.whereisplugin.api.v2.WhereisConfigAPI;
 import xyz.n7mn.dev.whereisplugin.api.v2.WhereisData;
